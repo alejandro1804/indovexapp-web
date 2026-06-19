90182bc78e3a2c072acb0e96b0bc7de7a29aace4
alejandro1804 <alejandro1804@gmail.com>
2026-06-19 01:25:36 -0300

    Build: agregar generación de QR por máquina

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2871476972" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1474984852" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
