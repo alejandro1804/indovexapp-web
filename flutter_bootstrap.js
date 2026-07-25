a1f5a0f308e8a1dc3c57460ec77d414576514a3c
alejandro1804 <alejandro1804@gmail.com>
2026-07-25 03:09:52 -0300

    Pantalla de notificaciones: campanita y lista

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2082053402" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1557978610" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
