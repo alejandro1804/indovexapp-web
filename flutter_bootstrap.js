be816d7ccd0006d1ba02a9331d5ee18dfce61627
alejandro1804 <alejandro1804@gmail.com>
2026-08-30 13:35:38 -0300

    Verificacion QR en transiciones de tickets, rescate admin, filtro ubicaciones, fixes UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "608565024" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1636183376" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
